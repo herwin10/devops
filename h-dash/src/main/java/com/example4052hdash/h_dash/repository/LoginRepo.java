e964be39ec2d0b9245899fecd6ccf02e7943e8f9
herwin10 <160164222+herwin10@users.noreply.github.com>
2025-03-24 02:48:41 -0300

    Delete h-dash directory

--- h-dash/src/main/java/com/example4052hdash/h_dash/repository/LoginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example4052hdash.h_dash.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoginRepo extends JpaRepository<login,String>{
-
-    login findByEmailAndPassword(String email,String password)
-}
--- h-dash/src/main/java/com/example4052hdash/h_dash/service/LoginService.java
+++
@@ -1,5 +0,0 @@
-package com.example4052hdash.h_dash.service;
-
-public class LoginService {
-
-}
